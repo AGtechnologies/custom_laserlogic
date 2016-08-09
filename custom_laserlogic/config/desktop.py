488323e96c3d3de8d1a5e00f6ec12b8b9cf53231
justinlusg <justin.lu@agtech.com.sg>
2016-08-09 23:54:24 +0800

    Updated

--- custom_laserlogic/config/desktop.py
+++ custom_laserlogic/config/desktop.py
@@ -5,10 +5,10 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Laser Logic",
+			"module_name": "Stock Forecasts",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("Laser Logic")
+			"label": _("Stock Forecasts")
 		}
 	]

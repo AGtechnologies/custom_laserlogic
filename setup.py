ffd81b17a7bfaaddaa8235b003d784fba8b8e1dd
justinlusg <justin.lu@agtech.com.sg>
2016-08-10 00:24:44 +0800

    Revert "Changes to app naming"
    This reverts commit 879aa2234109d6824043199de8b37a17b805ffa4.

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ from setuptools import setup, find_packages
 from pip.req import parse_requirements
 import re, ast
 
-# get version from __version__ variable in laserlogic/__init__.py
+# get version from __version__ variable in stock_forecasts/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('laserlogic/__init__.py', 'rb') as f:
+with open('stock_forecasts/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
-	name='custom_laserlogic',
+	name='stock_forecasts',
 	version=version,
-	description='Stock Forecast Report',
-	author='AG Technologies Pte Ltd',
-	author_email='info@agtech.com.sg',
+	description='Stock Forecasts',
+	author='AGT Technologies',
+	author_email='agt.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
--- stock_forecasts/config/desktop.py
+++ stock_forecasts/config/desktop.py
@@ -5,9 +5,10 @@ from frappe import _
 def get_data():
 	return [
 		{
+			"module_name": "Stock Forecasts",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("Laser Logic")
+			"label": _("Stock Forecasts")
 		}
 	]
